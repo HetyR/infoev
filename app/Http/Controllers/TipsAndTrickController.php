ab342fdf729b5ebe8a309543ce4c5e062500cea0
HetyRachmawati <168109641+HetyRachmawati@users.noreply.github.com>
2025-05-19 10:50:45 +0700

    lagilagi

--- app/Http/Controllers/TipsAndTrickController.php
+++ app/Http/Controllers/TipsAndTrickController.php
@@ -3,32 +3,114 @@
 namespace App\Http\Controllers;
 
 use App\Models\TipsAndTrick;
+use App\Models\Brand;
+use App\Models\Blog;
+use App\Models\Option;
+use App\Models\Vehicle;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class TipsAndTrickController extends Controller
 {
-    // Menampilkan daftar Tips and Trick
     public function index(Request $request)
     {
-        // Mengambil artikel Tips and Trick yang dipublikasikan dan mengurutkannya
-        $tipsAndTricks = TipsAndTrick::where('published', true)
-                                     ->latest()
-                                     ->paginate(10);
+        $tipsAndTricks = TipsAndTrick::whereHas('blog', fn($q) =>
+            $q->where('published', true)
+        )->with(['blog.thumbnail'])->latest()->paginate(10);
 
-        // Menampilkan tampilan dengan data artikel
+        $stickies = Blog::with('thumbnail')
+            ->select('sticky_articles.*', 'blogs.*')
+            ->join('sticky_articles', 'blogs.id', '=', 'sticky_articles.blog_id')
+            ->where('blogs.published', true)
+            ->orderBy('sticky_articles.created_at', 'desc')
+            ->get();
+
+        $featured = Blog::with('thumbnail')
+            ->latest()
+            ->where('published', true)
+            ->where('featured', true)
+            ->limit(3)
+            ->get();
+
+        $newsLimit = 3 - $featured->count();
+        if ($newsLimit > 0 && $newsLimit <= 3) {
+            $remainderArticles = Blog::with('thumbnail')
+                ->latest()->where('published', true)
+                ->limit($newsLimit)->get();
+
+            $stickies = $stickies->concat($featured)->concat($remainderArticles);
+        }
+
+        $banner = Option::where([
+            ['type', 'banner'],
+            ['name', 'tips']
+        ])->with('thumbnail')->first();
+
         return view('tips.index', [
-            'tipsAndTricks' => $tipsAndTricks
+            'tipsAndTricks' => $tipsAndTricks,
+            'bikeBrands' => Brand::limit(14)
+                ->whereHas('vehicles.type', fn (Builder $query) =>
+                    $query->where('name', 'sepeda motor'))
+                ->withCount('vehicles')->having('vehicles_count', '>', 0)
+                ->orderBy('vehicles_count', 'desc')->get(),
+
+            'carBrands' => Brand::limit(14)
+                ->whereHas('vehicles.type', fn (Builder $query) =>
+                    $query->where('name', 'mobil'))
+                ->withCount('vehicles')->having('vehicles_count', '>', 0)
+                ->orderBy('vehicles_count', 'desc')->get(),
+
+            'banner' => is_null($banner) || is_null($banner->thumbnail) ? null : $banner->thumbnail,
+            'stickies' => $stickies,
+            'recentVehicles' => Vehicle::with('brand')->latest()->limit(8)->get(),
+            'popularVehicles' => Vehicle::with('brand')
+                ->whereHas('views', fn (Builder $query) =>
+                    $query->where('created_at', '>', now()->subMonths(3)))
+                ->withCount('views')->orderBy('views_count', 'desc')->limit(10)->get(),
+            'logo' => Option::where('type', 'logo')->with('thumbnail')->first()
         ]);
     }
 
-    // Menampilkan detail Tips and Trick
     public function show($id)
     {
-        // Menampilkan artikel berdasarkan ID
         $tipsAndTrick = TipsAndTrick::findOrFail($id);
 
+        $stickies = TipsAndTrick::where('published', true)
+            ->where('is_sticky', true)->latest()->get();
+
+        $featured = TipsAndTrick::where('published', true)
+            ->where('featured', true)->latest()->limit(3)->get();
+
+        $newsLimit = 3 - $featured->count();
+        if ($newsLimit > 0 && $newsLimit <= 3) {
+            $remainder = TipsAndTrick::where('published', true)
+                ->latest()->limit($newsLimit)->get();
+
+            $stickies = $stickies->concat($featured)->concat($remainder);
+        }
+
         return view('tips.show', [
-            'tipsAndTrick' => $tipsAndTrick
+            'bikeBrands' => Brand::limit(14)
+                ->whereHas('vehicles.type', fn (Builder $q) =>
+                    $q->where('name', 'sepeda motor'))
+                ->withCount('vehicles')->having('vehicles_count', '>', 0)
+                ->orderBy('vehicles_count', 'desc')->get(),
+
+            'carBrands' => Brand::limit(14)
+                ->whereHas('vehicles.type', fn (Builder $q) =>
+                    $q->where('name', 'mobil'))
+                ->withCount('vehicles')->having('vehicles_count', '>', 0)
+                ->orderBy('vehicles_count', 'desc')->get(),
+
+            'tipsAndTrick' => $tipsAndTrick,
+            'stickies' => $stickies,
+            'recentVehicles' => Vehicle::with('brand')->latest()->limit(8)->get(),
+            'popularVehicles' => Vehicle::with('brand')
+                ->whereHas('views', fn (Builder $q) =>
+                    $q->where('created_at', '>', now()->subMonths(3)))
+                ->withCount('views')->orderBy('views_count', 'desc')->limit(10)->get(),
+            'logo' => Option::where('type', 'logo')->with('thumbnail')->first()
         ]);
     }
+    
 }
